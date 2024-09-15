1ffc252fb7cdb8671c716df31a19842859818fae
David Agbesinyale <davidm7040@gmail.com>
2024-09-15 02:56:14 -0700

    updating ui with container wrap

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-primary text-primary-content">
 <div className="containerWrap flex justify-between">
-  <button className="btn btn-ghost text-xl">daisyUI</button>
+  <button className="btn btn-ghost text-xl">My Real-Time Chatroom</button>
   <button>Logout</button>
 </div>
     </div>
