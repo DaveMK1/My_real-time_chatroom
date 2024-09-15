e406ff821b7d4d44098e11b20c7bea00c2d1b9df
David Agbesinyale <davidm7040@gmail.com>
2024-09-15 05:26:52 -0700

    update

--- vite-project/src/components/Send_message.jsx
+++ vite-project/src/components/Send_message.jsx
@@ -3,7 +3,7 @@ const Send_message = () => {
   return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
       <form className="containerWrap flex">
-        <input className="input w-full focus:outline-none bg-gray-100 rounded-r-none text-black-700" type="text" />
+        <input className="input w-full focus:outline-none bg-gray-100 rounded-r-none text-black" type="text" />
         <button className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">Send</button>
       </form>
     </div>
