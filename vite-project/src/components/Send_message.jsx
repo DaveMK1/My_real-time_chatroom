6bb51a06ef2625aef4e74244eac8a9cf62f90c49
David Agbesinyale <davidm7040@gmail.com>
2024-09-15 04:37:25 -0700

    add send button

--- vite-project/src/components/Send_message.jsx
+++ vite-project/src/components/Send_message.jsx
@@ -2,6 +2,10 @@
 const Send_message = () => {
   return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">Send_message</div>
+      <form>
+        <input type="text" />
+        <button>Send</button>
+      </form>
   )
 }
 
