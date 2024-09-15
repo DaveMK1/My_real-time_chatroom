93036fa572de59eda7b07d687f92348d0c8d4e6b
David Agbesinyale <davidm7040@gmail.com>
2024-09-14 17:40:23 -0700

    add button

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <button className="btn">Button</button>	  
     </>
   )
 }
 
-export default App
