40f93dec70da13a3e7aaee597c829a2968caaeb5
David Agbesinyale <davidm7040@gmail.com>
2024-09-15 05:35:04 -0700

    update

--- vite-project/src/components/Send_message.jsx
+++ vite-project/src/components/Send_message.jsx
@@ -1,9 +1,12 @@
 
 const Send_message = () => {
+const [value, setValue] = useState("");
+console.log(value);
+
   return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
       <form className="containerWrap flex">
-        <input className="input w-full focus:outline-none bg-gray-100 rounded-r-none text-black" type="text" />
+        <input alue={value} onChange={e => setValue(e.target.value)} className="input w-full focus:outline-none bg-gray-100 rounded-r-none text-black" type="text" />
         <button className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">Send</button>
       </form>
     </div>
