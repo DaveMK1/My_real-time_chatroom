a8144a3fc727f454c717eb4ddd3ad600abe48d5f
David Agbesinyale <davidm7040@gmail.com>
2024-09-14 18:48:24 -0700

    login update

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -4,12 +4,11 @@ const Login = () => {
 <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content text-center">
     <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
+      <h1 className="text-5xl font-bold">Welcome aboard! 🚀</h1>
       <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+        Connect, chat, and share experiences with a vibrant community—all in one space!
       </p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="btn btn-primary">Login With Google</button>
     </div>
   </div>
 </div>
