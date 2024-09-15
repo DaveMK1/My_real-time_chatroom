646da0b19f6790eaaf52c04c4bb020cf73e67b5f
David Agbesinyale <davidm7040@gmail.com>
2024-09-15 14:28:06 -0700

    setting up firebase

---
+++ vite-project/src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBudRaB6yPEC1FubO0WEJd6OIKdnw305vA",
+  authDomain: "my-real-time-chatroom-72791.firebaseapp.com",
+  projectId: "my-real-time-chatroom-72791",
+  storageBucket: "my-real-time-chatroom-72791.appspot.com",
+  messagingSenderId: "598637876443",
+  appId: "1:598637876443:web:a73fb4249eb4c0f79768fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
