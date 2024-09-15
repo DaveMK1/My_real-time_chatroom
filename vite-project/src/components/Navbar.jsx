d16cd8969a166c0bd6d85f9692e6ac76b8a7552a
David Agbesinyale <davidm7040@gmail.com>
2024-09-15 02:07:15 -0700

    update

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    <div className="navbar bg-neutral text-neutral-content">
+  <button className="btn btn-ghost text-xl">daisyUI</button>
+</div>
+<div className="navbar bg-base-300">
+  <button className="btn btn-ghost text-xl">daisyUI</button>
+</div>
+<div className="navbar bg-primary text-primary-content">
+  <button className="btn btn-ghost text-xl">daisyUI</button>
+</div>
+    </div>
   )
 }
 
