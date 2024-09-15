07c1e32fb62770deca9c301c8f752e81aadcd2e3
David Agbesinyale <davidm7040@gmail.com>
2024-09-15 05:55:16 -0700

    update

--- vite-project/src/components/Send_message.jsx
+++ vite-project/src/components/Send_message.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
 
 const Send_message = () => {
-const [value, setValue] = useState("");
-console.log(value);
+ const [value, setValue] = useState("");
 
-  return (
+
+ const handleSendMessage = () => {
+  console.log(value);
+ }
+
+ return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
-      <form className="containerWrap flex">
+      <form onSubmit={handleSendMessage} className="containerWrap flex">
         <input alue={value} onChange={e => setValue(e.target.value)} className="input w-full focus:outline-none bg-gray-100 rounded-r-none text-black" type="text" />
-        <button className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">Send</button>
+        <button type="submit" className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">Send</button>
       </form>
     </div>
   )
