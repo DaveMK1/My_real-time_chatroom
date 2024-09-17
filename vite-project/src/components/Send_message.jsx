19ed7a50a0e1992c4e063acca712230f6ff47d14
David Agbesinyale <davidm7040@gmail.com>
2024-09-17 10:03:28 -0700

    sending messages

--- vite-project/src/components/Send_message.jsx
+++ vite-project/src/components/Send_message.jsx
@@ -1,21 +1,45 @@
+
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useState } from "react"
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
 
-const Send_message = () => {
- const [value, setValue] = useState("");
+const SendMessage = () => {
+  const [value, setValue] = useState("");
+  const { currentUser } = UserAuth();
+  
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
 
- const handleSendMessage = (e) => {
- console.log(value);
-  e.preventDefault();
- }
+    if(value.trim() === "") {
+      alert("Enter valid message!");
+      return;
+    }
 
- return (
-    <div className="bg-gray-400 fixed bottom-0 w-full py-10 shadow-lg">
-      <form onSubmit={handleSendMessage} className="px-3 containerWrap flex">
-        <input alue={value} onChange={e => setValue(e.target.value)} className="input w-full focus:outline-none bg-gray-100 rounded-r-none text-black" type="text" />
+    try {
+      const { uid, displayName, photoURL } = currentUser; 
+      await addDoc(collection(db, "messages"), {
+        text: value,
+        name: displayName,
+        avatar: photoURL,
+        createdAt: serverTimestamp(),
+        uid
+      })
+    } catch(error) {
+      console.log(error);
+    }
+    setValue("");
+  }
+
+  return (
+    <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
+      <form onSubmit={handleSendMessage} className="px-2 containerWrap flex">
+        <input value={value} onChange={e => setValue(e.target.value)} className="input w-full focus:outline-none bg-gray-100 rounded-r-none" type="text" />
         <button type="submit" className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">Send</button>
       </form>
     </div>
   )
 }
 
-export default Send_message
+export default SendMessage
+
