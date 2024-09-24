12dc1e0da76b8c4c66b7ec4fc4f4eb0e847662a2
David Agbesinyale <davidm7040@gmail.com>
2024-09-24 05:31:31 -0700

    adding comments

--- vite-project/src/components/Message.jsx
+++ vite-project/src/components/Message.jsx
@@ -1,21 +1,27 @@
+// Import the authentication context to access the current user
 import { UserAuth } from "../context/Auth_Context";
 
 const Message = ({ message }) => {
-  const { currentUser } = UserAuth();
+  const { currentUser } = UserAuth(); // Get the current logged-in user
 
-  console.log(message);
   return (
     <div>
+      {/* Conditional class for message alignment (right for sender, left for others) */}
       <div className={`chat ${message.uid === currentUser.uid ? "chat-end" : "chat-start"}`}>
         <div className="chat-image avatar">
+          {/* Display the user's avatar image */}
           <div className="w-10 rounded-full">
-            <img src={message.avatar} alt={message.name} /> {/* Added alt for accessibility */}
+            <img src={message.avatar} alt={message.name} /> {/* Added alt attribute for accessibility */}
           </div>
         </div>
         <div className="chat-header">
+          {/* Display the name of the message sender */}
           {message.name}
         </div>
-        <div className="chat-bubble">{message.text}</div>
+        <div className="chat-bubble">
+          {/* Display the message text */}
+          {message.text}
+        </div>
       </div>
     </div>
   );
