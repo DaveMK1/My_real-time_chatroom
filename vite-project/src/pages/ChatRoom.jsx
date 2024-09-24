1a077e2b2c0d8819b8e0392297056492ced79e4b
David Agbesinyale <davidm7040@gmail.com>
2024-09-24 05:48:27 -0700

    adding comments

--- vite-project/src/pages/ChatRoom.jsx
+++ vite-project/src/pages/ChatRoom.jsx
@@ -1,10 +1,15 @@
+// Import the Chatbox and Send_message components
 import Chatbox from "../components/Chatbox"
 import Send_message from "../components/Send_message"
 
+// The ChatRoom component is the main chat interface, displaying both the chatbox and the send message input
 const ChatRoom = () => {
   return (
     <div>
+      {/* Chatbox displays all chat messages */}
       <Chatbox />
+      
+      {/* Send_message allows the user to type and send new messages */}
       <Send_message />
     </div>
   )
