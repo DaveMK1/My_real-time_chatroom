df994f663a805b17dd9928a6a54083f9632e8c98
David Agbesinyale <davidm7040@gmail.com>
2024-09-24 04:22:39 -0700

    adding comments

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
-// Import necessary modules from 'react-router-dom' for routing
 import { Route, Routes } from "react-router-dom";
-// Import components: Navbar, ChatRoom, Login, PrivateRoute, and AuthProvider for the app's main functionality
 import Navbar from "./components/Navbar";
 import ChatRoom from "./pages/ChatRoom";
 import Login from "./pages/Login";
@@ -8,21 +6,16 @@ import { PrivateRoute } from "./routes/PrivateRoute";
 import { AuthProvider } from "./context/Auth_Context";
 
 function App() {
-  // The main App component renders the Navbar and handles routing using 'Routes'.
-  // The AuthProvider wraps the app to provide authentication context to the entire app.
+
   return (
     <AuthProvider>
-      {/* Navbar component displayed on all pages */}
-      <Navbar />
-      {/* Define application routes */}
-      <Routes>
-        {/* Route for Login page, accessible at '/' */}
-        <Route path="/" element={<Login />} />
-        {/* PrivateRoute is used to protect the chat route, rendering ChatRoom only for authenticated users */}
-        <Route path="/chat" element={<PrivateRoute><ChatRoom /></PrivateRoute>} />
-      </Routes>
+     <Navbar />
+     <Routes>
+       <Route path="/" element={<Login />} />
+       <Route path="/chat" element={<PrivateRoute><ChatRoom /></PrivateRoute>} />
+     </Routes>
     </AuthProvider>
   )
 }
 
-export default App;
+export default App
